e6576d85e1c913478626e9585dac27fd5dd033a5
trueSolju <jusol0701@gmail.com>
2024-04-03 14:14:27 +0900

    tempReh

---
+++ src/main/java/com/green/WildHorse/travg/vo/TempRegAvgVO.java
@@ -0,0 +1,11 @@
+package com.green.WildHorse.travg.vo;
+
+import lombok.Data;
+
+@Data
+public class TempRegAvgVO {
+    private int trNum;
+    private String trDate;
+    private double tempAvg;
+    private double rehAvg;
+}
