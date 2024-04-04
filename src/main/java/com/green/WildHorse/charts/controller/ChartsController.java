56eb995429f8431744f863af8a45df45366374aa
gui4458 <gui4458@gmail.com>
2024-04-04 14:11:08 +0900

    dev

---
+++ src/main/java/com/green/WildHorse/charts/controller/ChartsController.java
@@ -0,0 +1,33 @@
+package com.green.WildHorse.charts.controller;
+
+import com.green.WildHorse.charts.service.ChartsService;
+import com.green.WildHorse.charts.vo.TempVO;
+import jakarta.annotation.Resource;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/charts")
+public class ChartsController {
+    @Resource(name = "chartsService")
+    private ChartsService chartsService;
+
+    @GetMapping("/main")
+    private String chartMain() {
+
+        return "content/main/main";
+    }
+
+    @ResponseBody
+    @PostMapping("/main")
+    private List<TempVO> getTemp() {
+        List<TempVO> tempsList = chartsService.selectDailyTemp();
+        return tempsList;
+    }
+}
---
+++ src/main/java/com/green/WildHorse/charts/vo/TempVO.java
@@ -0,0 +1,10 @@
+package com.green.WildHorse.charts.vo;
+
+import lombok.Data;
+
+@Data
+public class TempVO {
+    private String mesurDy;
+    private int maxTemp;
+    private int minTemp;
+}
